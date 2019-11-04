a7a1fbc9ad52e1e6d9f05dd1f3659b6c2e802293
richardcrng@gmail.com <richardcrng@gmail.com>
2019-11-04 20:01:18 +0000

    Format Lobby somewhat

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,19 @@ const App: React.FC = () => {
         <Lobby
           data={[
             { name: 'Game one', id: '243x' },
-            { name: 'second', id: 'xâ3' }
+            { name: 'second', id: 'xâ3' },
+            { name: 'second', id: 'xâ32' },
+            { name: 'second', id: 'xâ33' },
+            { name: 'second', id: 'xâ43' },
+            { name: 'second', id: 'xâ34' },
+            { name: 'second', id: 'xâ35' },
+            { name: 'second', id: 'xâ53' },
+            { name: 'second', id: 'xâ63' },
+            { name: 'second', id: 'xâ73' },
+            { name: 'second', id: 'xâ83' },
+            { name: 'second', id: 'xâ93' },
+            { name: 'second', id: 'xâ103' },
+            { name: 'second', id: 'xâ113' }
           ]}
         />
       </WingBlank>
--- src/app/modules/Lobby/Lobby.tsx
+++ src/app/modules/Lobby/Lobby.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import LobbyGames from './Games';
 import { Button } from 'antd-mobile';
+import CentreBottom from 'lib/atoms/CentreBottom';
 
 interface Props {
   data?: { name: string, id: string }[]
@@ -11,11 +12,19 @@ interface Props {
 function Lobby({ data, onHostNew, onGameClick } : Props) {
   return (
     <>
-      <LobbyGames
-        data={data}
-        onGameClick={onGameClick}
-      />
-      <Button onClick={onHostNew}>Host new</Button>
+      <h2>Games available</h2>
+      <p>Click on a game to join</p>
+      <div style={{ height: '300px', overflowY: 'scroll' }}>
+        <LobbyGames
+          data={data}
+          onGameClick={onGameClick}
+        />
+      </div>
+      <CentreBottom>
+        <Button onClick={onHostNew} type='primary'>
+          Host new
+        </Button>
+      </CentreBottom>
     </>
   )
 }
