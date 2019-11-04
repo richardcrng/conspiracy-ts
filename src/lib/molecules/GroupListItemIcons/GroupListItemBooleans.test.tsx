f23112ee7c0741544a5dabe6927206cca1afdce0
richardcrng@gmail.com <richardcrng@gmail.com>
2019-11-04 18:41:03 +0000

    Test that GroupListItemBooleans can pass down data

--- src/lib/molecules/GroupListItemIcons/GroupListItemBooleans.test.tsx
+++ src/lib/molecules/GroupListItemIcons/GroupListItemBooleans.test.tsx
@@ -15,7 +15,7 @@ let getByText: (text: Matcher, options?: MatcherOptions | undefined) => HTMLElem
 let getByTestId: (text: Matcher, options?: MatcherOptions | undefined) => HTMLElement
 
 describe('It converts an array of data into an appropriate list of elements', () => {
-  describe('GIVEN an array of data with no ids', () => {
+  describe('GIVEN an array of data', () => {
     beforeEach(() => {
       const data = [
         'Richard',
@@ -33,6 +33,55 @@ describe('It converts an array of data into an appropriate list of elements', ()
 
     test('AND it renders an icon for every element in the array', () => {
       expect(queryAllByTestId(container, 'icon-of-ListItemIcon')).toHaveLength(3)
+    })
+  })
+})
+
+describe('It can pass an onItemClick function down with appropriate data to pass as a second argument', () => {
+  describe('GIVEN an array of core data, onItemClickData and a callback function', () => {
+    let callback: jest.Mock
+
+    beforeEach(() => {
+      const data = ['Richard', 'Gregor', 'Jules'];
+      const onItemClickData = [4, 11, 7];
+      callback = jest.fn();
+      ({ container, getByText } = render(
+        <GroupListItemIcons<number>
+          data={data}
+          icon={IoMdAddCircle}
+          onItemClickData={onItemClickData}
+          onItemClick={callback}
+        />
+      ))
+    })
+
+    test('THEN it renders all of the text from the first element of each element in the array', () => {
+      expect(getByText('Richard')).toBeDefined()
+      expect(getByText('Gregor')).toBeDefined()
+      expect(getByText('Jules')).toBeDefined()
+    })
+
+    test('AND it renders an icon for every element in the array', () => {
+      expect(queryAllByTestId(container, 'icon-of-ListItemIcon')).toHaveLength(3)
+    })
+
+    describe('AND when the second data item is clicked on', () => {
+      let lastCallArgs: any[]
+      beforeEach(() => {
+        fireEvent.click(getByText('Gregor'))
+        lastCallArgs = callback.mock.calls[callback.mock.calls.length - 1]
+      })
+
+      test('THEN the callback has been called once with the onItemClickData passed in', () => {
+        expect(callback).toHaveBeenCalledTimes(1)
+        expect(lastCallArgs).toContain(11)
+        expect(lastCallArgs).not.toContain(4)
+        expect(lastCallArgs).not.toContain(7)
+      })
+
+      test("AND it is specifically the second argument that is the onItemClickData", () => {
+        expect(lastCallArgs[1]).toBe(11)
+      })
     })
   })
 })
