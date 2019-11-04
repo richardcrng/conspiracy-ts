d16736fd63f0bf91fcf61b27723c6ac86a4f83cb
richardcrng@gmail.com <richardcrng@gmail.com>
2019-11-04 18:45:47 +0000

    Accept icons prop in GroupListItemIcons

--- src/lib/molecules/GroupListItemIcons/GroupListItemIcons.tsx
+++ src/lib/molecules/GroupListItemIcons/GroupListItemIcons.tsx
@@ -5,20 +5,25 @@ import ListItemIcon from '../ListItemIcon';
 interface Props<T = any> {
   data: React.ReactNode[]
   icon?: IconType | React.FunctionComponent
+  icons?: (IconType | React.FunctionComponent)[]
   ids?: (string | number)[]
   onItemClick? (event?: React.MouseEvent, itemOnClickDatum?: T): void
   onItemClickData?: T[]
 }
 
-function GroupListItemIcons<T = any>({ data, icon, ids, onItemClick, onItemClickData }: Props<T>) {
+function GroupListItemIcons<T = any>({ data, icon, icons = [], ids = [], onItemClick, onItemClickData = [] }: Props<T>) {
+  const iconArray = icon
+    ? Array(data.length).fill(icon)
+    : icons
+
   return (
     <>
       {
         data.map((children, index) => (
           <ListItemIcon<T>
-            key={ids && ids[index] ? ids[index] : String(children) + String(index)}
-            icon={icon}
-            onClickDatum={onItemClickData ? onItemClickData[index] : undefined}
+            key={ids[index] ? ids[index] : String(children) + String(index)}
+            icon={iconArray[index]}
+            onClickDatum={onItemClickData[index]}
             onClick={onItemClick}
           >
             {children}
