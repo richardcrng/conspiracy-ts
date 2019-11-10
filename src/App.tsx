ef3ec4778abdb37f8f57ddbcc712944e1f1fbc66
richardcrng@gmail.com <richardcrng@gmail.com>
2019-11-10 18:58:10 +0000

    Change Role text

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,14 @@ import React from 'react';
 import './App.css';
 import 'antd-mobile/dist/antd-mobile.css'
 import { WingBlank } from 'antd-mobile';
-import LobbyRoom from 'app/modules/Lobby/Room';
+import Role from 'lib/organisms/Role';
 
 const App: React.FC = () => {
-  const [state, setState] = React.useState(false)
-  const players = [
-    { id: 'f23f0', name: 'Richard', isReady: true, isHost: true },
-    { id: 'g23f0', name: 'Jenny', isReady: state },
-    { id: 'h23f0', name: 'Isabella', isReady: true }
-  ]
 
   return (
     <div className="App">
       <WingBlank>
-        <LobbyRoom
-          clientPlayer={players[1]}
-          onClientStatusChange={() => setState(prevState => !prevState)}
-          players={players}
-        />
+        <Role inAConspiracyAgainst='Angelo' />
       </WingBlank>
     </div>
   );
--- src/lib/organisms/Role/Role.tsx
+++ src/lib/organisms/Role/Role.tsx
@@ -3,16 +3,24 @@ import ModalTrigger from 'lib/molecules/ModalTrigger';
 import { Button } from 'antd-mobile';
 
 interface Props {
-  inAConspiracyAgainst?: string
+  inAConspiracyAgainst?: string | null
 }
 
 function Role({ inAConspiracyAgainst } : Props) {
+  const title = inAConspiracyAgainst
+    ? 'CONSPIRATOR'
+    : 'INNOCENT'
 
   return (
     <>
       <ModalTrigger
         buttons={[{ text: 'Hide role' }]}
-        title='Your role'
+        title={(
+          <>
+            Your role:
+            <h3>{title}</h3>
+          </>
+        )}
         message={<RoleDeclare inAConspiracyAgainst={inAConspiracyAgainst} />}
       >
         <Button>
@@ -25,9 +33,24 @@ function Role({ inAConspiracyAgainst } : Props) {
 
 function RoleDeclare({ inAConspiracyAgainst } : Props) {
   if (inAConspiracyAgainst) {
-    return <p>You are in a <b>Conspiracy</b> against <b>{inAConspiracyAgainst}</b></p>
+    return (
+      <>
+        <p><b>{inAConspiracyAgainst}</b> is your conspiracy victim.</p>
+        <p>Everybody else is a fellow conspirator.</p>
+        <p><i>Don't let on...</i></p>
+      </>
+    )
   } else {
-    return <p>You are <b>Innocent</b></p>
+    return (
+      <>
+        <p>This means one of two things.</p>
+        <ol>
+          <li>All other players in the game are also innocent...</li>
+          <li>... or they're all conspiring together against you!</li>
+        </ol>
+        <p><i>Stay vigilant!</i></p>
+      </>
+    )
   }
 }
 
